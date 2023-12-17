a5e62c8d7bdfcc504f36db3654c969751a68fd86
Michael Cheneval <michael@cheneval.com>
2023-12-17 19:00:05 +0100

    Test change

--- index.php
+++ index.php
@@ -106,7 +106,7 @@ $items = $db->query($selectQuery);
       <div class="navbar navbar-dark bg-dark shadow-sm">
         <div class="container d-flex justify-content-between">
           <a href="#" class="navbar-brand d-flex align-items-center">
-            <strong>TodoList</strong>
+            <strong>Better TodoList</strong>
           </a>
         </div>
       </div>
